8081b8c2eedebce241693287a9df92bb048d93a6
Sanjjushri <sanjjushrivarshini@gmail.com>
2022-01-07 09:55:03 +0530

    Hello World program

---
+++ Hello_world.java
@@ -0,0 +1,10 @@
+// package com.codewithsanjju;
+
+// main method will always use static
+
+public class Hello_world{
+    
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+    }
+}
