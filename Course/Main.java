66d8070361435e55241295074be1c9e3498941f9
Sanjjushri <sanjjushrivarshini@gmail.com>
2024-04-14 21:03:33 +0530

    added

--- Course/Main.java
+++ Course/Main.java
@@ -3,6 +3,29 @@ package Course;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.print("I love pizza");
+        System.out.println("It's really good!");
+        System.out.print("Does this print in new line?");
+        // escape sequencee for new line 
+        System.out.print("hii the next one should be in new line\n");
+        System.out.print("need to be in new line period");
+        System.out.println("1\n");
+        System.out.print("\t2");
+        // if you want your text within double quotes 
+        System.out.println("\"I love\" to explore");
+    
+        //  If you want to use backslashes in the output 
+        System.out.println("\\hahaha");
+
+        /*
+         * 
+         * 
+         * 
+         * This is for multi-line comments
+         */
+
+
+        //  VARIABLES:
+        
     }
 }
