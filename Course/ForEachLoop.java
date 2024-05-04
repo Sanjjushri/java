20888039b521eca7c539edaf96682ba7e674b177
Sanjjushri <sanjjushrivarshini@gmail.com>
2024-05-04 23:52:34 +0530

    added for each loop java program

--- Course/ForEachLoop.java
+++ Course/ForEachLoop.java
@@ -1,7 +1,29 @@
 package Course;
 
+import java.util.ArrayList;
+
 public class ForEachLoop {
     public static void main(String[] args){
         
+        // for-each = traversing technique to iterate through the elements in an array/collection 
+        //            less steps, more readable 
+        //            less flexible 
+
+        String[] animals = {"cat", "dog", "rat", "bird"};
+
+        for(String i: animals){
+            System.out.println(i);
+        }
+
+        ArrayList<String> animals_list = new ArrayList<String>();
+
+        animals_list.add("rat");
+        animals_list.add("cat");
+        animals_list.add("dog");
+        
+        for(String i : animals_list){
+            System.out.println(i);
+        }
+
     }
 }
---
+++ Course/Methods.java
@@ -0,0 +1,14 @@
+package Course;
+
+public class Methods {
+    public static void main(String[] args){
+
+        // method = a block of code that is executed whenever it is called upon 
+        
+        hello();
+    }
+
+    static void hello() {
+        System.out.println("Hello");
+    }
+}
